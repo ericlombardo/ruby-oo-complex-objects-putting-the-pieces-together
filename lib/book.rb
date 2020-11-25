6a8d205ea06c87dbc3bb8dd7a7e398ff676f5844
Eric <eric.m.lombardo@gmail.com>
2020-11-25 09:24:46 -0500

    commit tree 003b5c4cb1aefc7b98d42e1da289a4637b787660

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,22 @@
+# create Book class
+# give properties like title, author, page count
+# keep track of all genres of Books we create
+# turn own pages
 
+class Book  
+  # set reader and writer for variables
+  attr_accessor :author, :page_count, :genre
+  attr_accessor :title
+  # get title upon creation of book
+  def initialize(title)
+    # link title to this instance of the Class
+    @title = title
+  end
+
+  # puts message when "page is turned"
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
-# Make your shoe class here!
+# create Shoe class
+# initialize w/ brand
+# create setter and getter methods for brand, color, size, material, condition
+# create cobble method
+  # says shoe has been repaired
+  # sets condition to new
+
+class Shoe
+  # make reader and writer methods for instance variables
+  attr_accessor :brand, :color, :size, :material, :condition
+  # collect brand when instantiated
+  def initialize(brand)
+    @brand = brand
+  end
+  # puts shoe is fixed and changes condition
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
